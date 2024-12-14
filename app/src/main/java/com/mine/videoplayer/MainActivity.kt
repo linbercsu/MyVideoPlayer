2bb21958cd2f84a78e3274c74cb6ec8912a35360
Zhaolinlin <linlin.zhao@mxplayer.in>
2024-12-14 11:19:20 +0800

    native project.

--- app/src/main/java/com/mine/videoplayer/MainActivity.kt
+++ app/src/main/java/com/mine/videoplayer/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.mine.videoplayer
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -16,6 +17,11 @@ import com.mine.videoplayer.ui.theme.MyVideoPlayerTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val string = stringFromJNI()
+
+        Log.d("MainActivity", string)
+
         enableEdgeToEdge()
         setContent {
             MyVideoPlayerTheme {
@@ -27,6 +33,16 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+
+    companion object {
+        init {
+            System.loadLibrary("nativelib")
+        }
+
+        @JvmStatic
+        external fun stringFromJNI(): String
     }
 }
 
